47054e1593f4128d179436b92fe0ce61a9d18bd4
Ednah Bridget <bidgetkakah@gmail.com>
2025-07-16 00:37:30 +0300

    Add guess the number game with match case

---
+++ guess_the_number_with_match_case.py
@@ -0,0 +1,19 @@
+import random
+
+play_again = "yes"
+
+while play_again.lower() == "yes":
+    secret_number = random.randint(1, 5)
+    guess = int(input("Guess the secret number (between 1 and 10): "))
+
+    match guess:
+        case _ if guess == secret_number:
+            print("Congratulations, you guessed right!")
+        case _ if guess > secret_number:
+            print("Oops, your guess is a bit high. Try again!")
+        case _ if guess < secret_number:
+            print("Nope, your guess is a bit low. Give it another shot!")
+    
+    play_again = input("Would you like to play again? (yes/no): ")
+
+print("Thank you for playing. See you later!")
